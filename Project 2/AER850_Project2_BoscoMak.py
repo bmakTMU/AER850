0be64e030879354e6ca00ddf711f173ed2341492
bmakTMU <bosco.mak@torontomu.ca>
2025-10-28 16:30:38 -0400

    Project Initializiation
    - imported project files (Project outline, project data)
    - initialized project code (created file, divided sections)

---
+++ Project 2/AER850_Project2_BoscoMak.py
@@ -0,0 +1,19 @@
+""" AER850 Project 2 - Bosco Mak"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from tensorflow import keras
+from tensorflow.keras import layers
+
+""" 1. Data Processing """
+
+""" 2. Neural Network Architecture Design """
+
+""" 3. Hyperparameter Analysis """
+
+""" 4. Model Evaluation """
+
+""" 5. Model Testing """
+
